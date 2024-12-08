9cf2b791795f85a68050538268af637c7ee54b04
jhayashi1 <jaredhayashi@gmail.com>
2024-12-08 14:49:06 -0600

    chore: readd ecr

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,31 +1,31 @@
-# resource "aws_ecr_repository" "berry_bot_repo" {
-#   name = "berry-bot"
+resource "aws_ecr_repository" "berry_bot_repo" {
+  name = "berry-bot"
 
-#   image_tag_mutability = "IMMUTABLE"
-#   image_scanning_configuration {
-#     scan_on_push = true
-#   }
-# }
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
 
-# resource "aws_ecr_lifecycle_policy" "berry_bot_lifecycle" {
-#   repository = aws_ecr_repository.berry_bot_repo.name
+resource "aws_ecr_lifecycle_policy" "berry_bot_lifecycle" {
+  repository = aws_ecr_repository.berry_bot_repo.name
 
-#   policy = <<EOF
-# {
-#     "rules": [
-#         {
-#             "rulePriority": 1,
-#             "description": "Keep last 5 images",
-#             "selection": {
-#                 "tagStatus": "any",
-#                 "countType": "imageCountMoreThan",
-#                 "countNumber": 5
-#             },
-#             "action": {
-#                 "type": "expire"
-#             }
-#         }
-#     ]
-# }
-# EOF
-# }
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 5 images",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 5
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
