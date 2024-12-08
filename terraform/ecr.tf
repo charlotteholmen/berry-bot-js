42ddbd41f0441781c841f5fb5eefeaad05c84c17
jhayashi1 <jaredhayashi@gmail.com>
2024-12-08 14:38:12 -0600

    fix: proper lifecycle policy

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -5,23 +5,27 @@ resource "aws_ecr_repository" "berry_bot_repo" {
   image_scanning_configuration {
     scan_on_push = true
   }
+}
 
-  lifecycle_policy {
-    policy = jsonencode({
-      rules = [
+resource "aws_ecr_lifecycle_policy" "bery_bot_lifecycle" {
+  repository = aws_ecr_repository.berry_bot_repo.name
+
+  policy = <<EOF
+{
+    "rules": [
         {
-          rulePriority = 1
-          description  = "retain last 10 images"
-          selection    = {
-            tagStatus = "any"
-            countType = "imageCountMoreThan"
-            count     = 10
-          }
-          action = {
-            type = "expire"
-          }
+            "rulePriority": 1,
+            "description": "expire images older than 10 days",
+            "selection" = {
+              "tagStatus" = "any"
+              "countType" = "imageCountMoreThan"
+              "count"     = 10
+            }
+            "action": {
+                "type": "expire"
+            }
         }
-      ]
-    })
-  }
+    ]
+}
+EOF
 }
