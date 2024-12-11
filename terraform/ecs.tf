28972ed1442bc0d81d7dade406c793e3afd285c8
jhayashi1 <jaredhayashi@gmail.com>
2024-12-10 20:29:49 -0600

    fix: add cpu and memory to task def

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -93,12 +93,16 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["EC2"]
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  cpu                      = "256"
+  memory                   = "512"
 
   container_definitions = jsonencode([
     {
       name      = "berry-bot-container",
       image     = "${var.aws_account_id}.dkr.ecr.us-east-1.amazonaws.com/berry-bot:latest",
       essential = true,
+      cpu       = 256,
+      memory    = 512,
       portMappings = [
         {
           containerPort = 80
@@ -109,15 +113,15 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
   ])
 }
 
-# resource "aws_ecs_service" "ecs_service" {
-#   name            = "my-service"
-#   cluster         = aws_ecs_cluster.berry_bot_cluster.id
-#   task_definition = aws_ecs_task_definition.ecs_task_definition.arn
-#   desired_count   = 1
-#   launch_type     = "EC2"
-#   network_configuration {
-#     subnets          = data.aws_subnets.ipv6_subnets.ids
-#     security_groups  = [aws_security_group.berry_bot_ecs_sg.id]
-#     assign_public_ip = false
-#   }
-# }
+resource "aws_ecs_service" "ecs_service" {
+  name            = "my-service"
+  cluster         = aws_ecs_cluster.berry_bot_cluster.id
+  task_definition = aws_ecs_task_definition.ecs_task_definition.arn
+  desired_count   = 1
+  launch_type     = "EC2"
+  network_configuration {
+    subnets          = data.aws_subnets.ipv6_subnets.ids
+    security_groups  = [aws_security_group.berry_bot_ecs_sg.id]
+    assign_public_ip = false
+  }
+}
